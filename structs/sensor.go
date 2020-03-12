2da72421bb50a6e4018b7d0db6d3284f31cc5952
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-12 12:59:50 -0400

    Added new sensor struct

--- structs/sensor.go
+++ structs/sensor.go
@@ -14,3 +14,11 @@ type Sensor struct {
 	SoilMoistureValue   int                `json:"soilMoistureValue" bson:"soilMoistureValue"`
 	SoilMoisturePercent int                `json:"soilMoisturePercent" bson:"soilMoisturePercent"`
 }
+
+type SensorInfo struct {
+	SensorName          string `json:"sensorName" bson:"sensorName"`
+	AirValue            int    `json:"airValue" bson:"airValue"`
+	WaterValue          int    `json:"waterValue" bson:"waterValue"`
+	SoilMoistureValue   int    `json:"soilMoistureValue" bson:"soilMoistureValue"`
+	SoilMoisturePercent int    `json:"soilMoisturePercent" bson:"soilMoisturePercent"`
+}
