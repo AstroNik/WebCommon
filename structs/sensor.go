91598e540be33436c699896d4f34e78b783a777d
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-12 13:19:31 -0400

    changed time to string

--- structs/sensor.go
+++ structs/sensor.go
@@ -2,7 +2,6 @@ package structs
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"time"
 )
 
 type Sensor struct {
@@ -11,10 +10,10 @@ type Sensor struct {
 }
 
 type SensorData struct {
-	DateTime            time.Time `json:"dateTime" bson:"dateTime"`
-	SensorName          string    `json:"sensorName" bson:"sensorName"`
-	AirValue            int       `json:"airValue" bson:"airValue"`
-	WaterValue          int       `json:"waterValue" bson:"waterValue"`
-	SoilMoistureValue   int       `json:"soilMoistureValue" bson:"soilMoistureValue"`
-	SoilMoisturePercent int       `json:"soilMoisturePercent" bson:"soilMoisturePercent"`
+	DateTime            string `json:"dateTime" bson:"dateTime"`
+	SensorName          string `json:"sensorName" bson:"sensorName"`
+	AirValue            int    `json:"airValue" bson:"airValue"`
+	WaterValue          int    `json:"waterValue" bson:"waterValue"`
+	SoilMoistureValue   int    `json:"soilMoistureValue" bson:"soilMoistureValue"`
+	SoilMoisturePercent int    `json:"soilMoisturePercent" bson:"soilMoisturePercent"`
 }
