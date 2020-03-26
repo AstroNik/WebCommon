7772b74d3e058fb569fd8a07599c776f1a831c8d
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-26 16:41:26 -0400

    update db

--- db/db.go
+++ db/db.go
@@ -21,7 +21,7 @@ func InsertMoistureData(customerId string, sensor structs.Sensor) {
 	log.Println("Sensor Data: ", sensor)
 	client := ConnectClient()
 	//eventually user will be passed in this function as an ID
-	col := client.Database(customerId).Collection("SensorData")
+	col := client.Database("234556314").Collection("SensorData")
 	_, err := col.InsertOne(context.TODO(), sensor)
 	if err != nil {
 		log.Println("Cannot insert document")
