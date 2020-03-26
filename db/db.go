45ea300b0607d20116bbcc9f08a2e9923a59c241
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-26 17:16:34 -0400

    fixing connection issue

--- db/db.go
+++ db/db.go
@@ -6,14 +6,15 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"time"
 )
 
 func ConnectClient() *mongo.Client {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://devTeam:ecoders4@cluster0-grjmu.azure.mongodb.net/admin"))
+	clientOption := options.Client().ApplyURI("mongodb+srv://devTeam:ecoders4@cluster0-grjmu.azure.mongodb.net/admin")
+	client, err := mongo.Connect(context.TODO(), clientOption)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("mongo.Connect() ERROR: ", err)
 	}
-	log.Print("Connection Successful\n")
 	return client
 }
 
@@ -22,32 +23,13 @@ func InsertMoistureData(customerId string, sensor structs.Sensor) {
 	client := ConnectClient()
 	//eventually user will be passed in this function as an ID
 	col := client.Database(customerId).Collection("SensorData")
-	_, err := col.InsertOne(context.TODO(), &sensor)
+	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
+	_, err := col.InsertOne(ctx, &sensor)
 	if err != nil {
 		log.Println("Cannot insert document ERROR: ", err)
 	}
-}
 
-//func ConnectClient() *mongo.Client {
-//	clientOption := options.Client().ApplyURI("mongodb+srv://devTeam:ecoders4@cluster0-grjmu.azure.mongodb.net/admin")
-//	client, err := mongo.Connect(context.TODO(),clientOption)
-//	if err != nil {
-//		log.Println("mongo.Connect() ERROR: ", err)
-//	}
-//	return client
-//}
-//
-//func InsertMoistureData(customerId string, sensor structs.Sensor) {
-//	log.Println("Sensor Data: ", sensor)
-//	client := ConnectClient()
-//	//eventually user will be passed in this function as an ID
-//	col := client.Database(customerId).Collection("SensorData")
-//	_, err := col.InsertOne(context.TODO(), &sensor)
-//	if err != nil {
-//		log.Println("Cannot insert document ERROR: ", err)
-//	}
-//
-//}
+}
 
 //func GetMoistureData() {
 //	client := ConnectClient()
