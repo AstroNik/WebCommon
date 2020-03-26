a9b77dc603daba6cf167d11337f46437d50fab4f
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-26 16:27:07 -0400

    updated sensor

--- structs/sensor.go
+++ structs/sensor.go
@@ -1,18 +1,19 @@
 package structs
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
 type Sensor struct {
-	//ID         primitive.ObjectID `bson:"_id,omitempty"`
-	SensorId            int       `bson:"sensorId" json:"sensorId"`
-	SensorName          string    `bson:"SensorName" json:"sensorName"`
-	DateTime            time.Time `bson:"dateTime" json:"dateTime"`
-	AirValue            int       `bson:"airValue" json:"airValue"`
-	WaterValue          int       `bson:"waterValue" json:"waterValue"`
-	SoilMoistureValue   int       `bson:"soilMoistureValue" json:"soilMoistureValue"`
-	SoilMoisturePercent int       `bson:"soilMoisturePercent" json:"soilMoisturePercent"`
+	ID                  primitive.ObjectID `bson:"_id,omitempty"`
+	SensorId            int                `bson:"sensorId" json:"sensorId"`
+	SensorName          string             `bson:"SensorName" json:"sensorName"`
+	DateTime            time.Time          `bson:"dateTime" json:"dateTime"`
+	AirValue            int                `bson:"airValue" json:"airValue"`
+	WaterValue          int                `bson:"waterValue" json:"waterValue"`
+	SoilMoistureValue   int                `bson:"soilMoistureValue" json:"soilMoistureValue"`
+	SoilMoisturePercent int                `bson:"soilMoisturePercent" json:"soilMoisturePercent"`
 }
 
 type SensorData struct {
