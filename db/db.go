8fffd07cef2169c2e43d8fa4603a6199d0775ea0
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-26 20:43:09 -0400

    potential fix, data being retrieved is in wrong order

--- db/db.go
+++ db/db.go
@@ -35,9 +35,8 @@ func GetMoistureData(customerId string) structs.Sensor {
 	col := client.Database(customerId).Collection("SensorData")
 
 	filter := options.Find()
-	filter.SetLimit(1)
 	filter.SetSort(map[string]int{"datetime": -1})
-
+	filter.SetLimit(1)
 	cur, err := col.Find(context.TODO(), bson.D{{}}, filter)
 	if err != nil {
 		log.Println("Cannot retrieve document ERROR: ", err)
