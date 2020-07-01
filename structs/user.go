3141f0ab09e5e62256681e60816ce36b9f3a1e6e
Nikhil <nikhil.kapadia.97@gmail.com>
2020-07-01 00:32:18 -0400

    Updated User Struct

--- structs/user.go
+++ structs/user.go
@@ -5,16 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
 	UID       string             `json:"uid" bson:"uid"`
+	Email     string             `json:"email" bson:"email"`
 	FirstName string             `json:"firstName" bson:"firstName"`
 	LastName  string             `json:"LastName" bson:"lastName"`
-	Email     string             `json:"email" bson:"email"`
-	DeviceId  []int              `json:"deviceId" bson:"deviceId"`
 	Token     string             `json:"token" bson:"token"`
-}
-
-type NewUser struct {
-	UID       string `json:"uid" bson:"uid"`
-	Email     string `json:"email" bson:"email"`
-	FirstName string `json:"firstName" bson:"firstName"`
-	LastName  string `json:"LastName" bson:"lastName"`
 }
