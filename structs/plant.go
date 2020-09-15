78816fc83cb5bd32989532752b7c49d629e92b65
Nikhil <nikhil.kapadia.97@gmail.com>
2020-09-15 15:12:44 -0400

    Changed Plant Struct

--- structs/plant.go
+++ structs/plant.go
@@ -9,17 +9,13 @@ type Plant struct {
 	BotanicalName string             `json:"botanicalName" bson:"botanicalName"` //Ex. Epipremnum aureum
 	CommonName    string             `json:"commonName" bson:"commonName"`       //Ex. Golden pothos, silver vine, taro vine
 	PlantType     string             `json:"plantType" bson:"plantType"`         //Ex. Trailing Vine
-	Description   string             `json:"description" bson:"description"`     // Facts about plants
-	GrowthInfo    GrowthInfo         `json:"growthInfo" bson:"growthInfo"`
+	MatureSize    string             `json:"matureSize" bson:"matureSize"`       // Facts about plants
+	Care          Care               `json:"growthInfo" bson:"growthInfo"`
 }
 
-type GrowthInfo struct {
-	LightLevel string   `json:"lightLevel" bson:"lightLevel"` //Sun Exposure. Indirect / Direct
-	Humidity   string   `json:"humidity" bson:"humidity"`     //Room humidity
-	Watering   Watering `json:"watering" bson:"watering"`
-}
-
-type Watering struct {
-	Duration  string `json:"duration" bson:"duration"`   //How often
-	Direction string `json:"direction" bson:"direction"` //Near roots or far?
+type Care struct {
+	LightLevel string `json:"lightLevel" bson:"lightLevel"` //Sun Exposure. Indirect / Direct
+	Humidity   string `json:"humidity" bson:"humidity"`     //Room humidity
+	Duration   string `json:"duration" bson:"duration"`     //How often
+	Direction  string `json:"direction" bson:"direction"`   //Near roots or far?
 }
